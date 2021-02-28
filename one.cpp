0eae2261a477dfaf07a3a1169da6a7c1281eeada
Tushar Saxena <stushar345@gmail.com>
2021-02-28 17:36:10 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -9,11 +9,13 @@ bool areIsomorphic(string s1, string s2)
     int m2[256]={0};
     for(int i=0;i<n1;i++)
     {
-        if(!m1[s1[i]] && !m2[s2[i]]){
+        if(!m1[s1[i]] && !m2[s2[i]])                            //when value of that character in the map is 0 i.e it is the first variable
+        {
             m1[s1[i]] = s2[i];
             m2[s2[i]] = s1[i];
         }
-        else if(m1[s1[i]]!= s2[i]) return false;
+        else if(m1[s1[i]]!= s2[i])                              //It means that it's value is already present in the map just use the value
+            return false;
     }
     return true;
 }
